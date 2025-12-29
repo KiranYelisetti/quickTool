1eeaa471aa9bd85a677aac648ca8cee5f1ed675a
KiranYelisetti <ravikiranyelisetti18@gmail.com>
2025-12-29 08:56:52 +0530

    Migrate to Eleventy V3, enhance UI with borders, and optimize SEO

---
+++ eleventy.config.js
@@ -0,0 +1,21 @@
+module.exports = function (eleventyConfig) {
+    // Pass through static assets
+    eleventyConfig.addPassthroughCopy("src/favicon.svg");
+    eleventyConfig.addPassthroughCopy("src/robots.txt");
+    eleventyConfig.addPassthroughCopy("src/CNAME");
+    eleventyConfig.addPassthroughCopy("src/og-image.png");
+
+    // Watch targets
+    eleventyConfig.addWatchTarget("./src/css/");
+
+    return {
+        dir: {
+            input: "src",
+            output: "_site",
+            includes: "_includes"
+        },
+        templateFormats: ["html", "njk", "md"],
+        htmlTemplateEngine: "njk",
+        markdownTemplateEngine: "njk"
+    };
+};
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{html,js,njk}"],
+    theme: {
+        extend: {},
+        container: {
+            center: true,
+            padding: '1rem',
+        },
+    },
+    plugins: [],
+    darkMode: 'class',
+}
