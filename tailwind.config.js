a1f571392b7a1a87b6c6ff5e3f2e6e1ba78f204c
Kiran Yelisetti <ravikiranyelisetti18@gmail.com>
2026-04-10 13:34:42 +0530

    Fixed: Added Tailwind safelist for emerald/teal/amber/cyan colors used in new calculators

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
 module.exports = {
     content: ["./src/**/*.{html,js,njk}"],
+    safelist: [
+        { pattern: /from-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /to-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /bg-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /text-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /border-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /ring-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /hover:bg-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /hover:text-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /focus:ring-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /dark:bg-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /dark:text-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+        { pattern: /dark:border-(emerald|teal|cyan|amber)-(50|100|200|300|400|500|600|700|800|900)/ },
+    ],
     theme: {
         extend: {},
         container: {
